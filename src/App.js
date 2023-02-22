3425bc9f0f4ebb04806b8ae5e127c59f589505ff
Iqbal Erani <iqbalerani@gmail.com>
2023-02-22 19:33:57 +0500

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  /**
+   * Challenge: See if you can do it all again by yourself :)
+   */
+
+  const [thingsArray, setthingsArray] = React.useState(["Thing 1", "Thing 2"]);
+
+  function addItem() {
+    // Build from scratch :)
+    setthingsArray((prevState) => {
+      return [...prevState, `Things ${prevState.length + 1}`];
+    });
+  }
+  // const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"])
+
+  // function addItem() {
+  //     // We'll work on this next
+  //     setThingsArray(prevThingsArray => {
+  //         return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`]
+  //     })
+  // }
+  const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      {thingsElements}
     </div>
   );
 }
